322a987a86eaa4407670f2d01876acacf79a8c4b
dezguell <dezguell@gmail.com>
2022-02-04 18:19:11 -0300

    ussing propper conditions

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,20 @@ use std::io;
 use std::cmp::Ordering;
 
 const HINT_STR_VALUE: &str = "-hint";
+const MSG_PREFIX: &str = "The number you guessed was";
+const MSG_AND_IT_IS_A: &str = "and it is a";
+const MSG_WRONG_GUESS: &str = "wrong guess, the number is to";
+const MSG_RIGHT_GUESS: &str = "right guess, you win!";
+const MSG_YOU_LOSS: &str = "you loss";  
+const CHECK_CONDITION_TO_CONTINUE: &u32 = &u32::MIN;
 
-fn main() {
-
-    const MSG_PREFIX: &str = "The number you guessed was";
-    const MSG_AND_IT_IS_A: &str = "and it is a";
-    const MSG_WRONG_GUESS: &str = "wrong guess, the number is to";
-    const MSG_RIGHT_GUESS: &str = "right guess, you win!";
-    const MSG_YOU_LOSS: &str = "you loss";       
+fn main() {      
 
     println!("This is the Guess The Number Game.");
     println!();    
     
     let number_to_guess = rand::thread_rng().gen_range(1..101);    
     //println!("The number to guess is {}", number_to_guess);   
-
     
     loop{        
         println!("Please enter the number between 1 and 101!.");
@@ -27,7 +26,7 @@ fn main() {
             .read_line(&mut guess_str)
             .expect("Fail to read the number");           
         
-        let mut guess_int: u32 = 0;
+        let mut guess_int: u32;
 
         if check_hint_request(&guess_str){
             guess_str = guess_str
@@ -35,24 +34,23 @@ fn main() {
                 .trim()
                 .to_string();
                         
-            guess_int = convert_str_to_int(&guess_str);
+            guess_int = convert_str_to_int(&mut guess_str);
             
-            if guess_int.eq(&u32::MIN){
+            if guess_int.eq(CHECK_CONDITION_TO_CONTINUE){
                 continue;
             }  
 
             println!("-------- Hint ----------");
             let _hint_result = number_to_guess - guess_int;
             println!("The number to guess minus the number you entered is equal: {}", _hint_result);
+            println!("------------------------");
         }
         
-        if guess_int.eq(&0){
-            guess_int = convert_str_to_int(&guess_str); 
-        }
-        
-        if guess_int.eq(&u32::MIN){
+        guess_int = convert_str_to_int(&mut guess_str);         
+
+        if guess_int.eq(CHECK_CONDITION_TO_CONTINUE) {
             continue;
-        }  
+        } 
         
         match guess_int.cmp(&number_to_guess){
             Ordering::Less => get_loss_by_less(&guess_str),
@@ -87,11 +85,14 @@ fn check_hint_request( _text: &std::string::String) -> bool{
     false
 }
 
-fn convert_str_to_int(_text: &std::string::String) -> u32 {
-    match _text.trim().parse()
+fn convert_str_to_int(_text: &mut std::string::String) -> u32 {
+    if _text.eq(&"0"){
+        return *CHECK_CONDITION_TO_CONTINUE;
+    }    
+    match _text.trim().parse::<u32>()
             {
                 Ok(num) => num,
-                Err(_) => u32::MIN,
+                Err(_) => *CHECK_CONDITION_TO_CONTINUE,                
             }    
 }
 
